625e613c9e30bdf73ea2de9c1b452cb99203e418
Vishal Urankar <68900436+vvu060@users.noreply.github.com>
2020-07-28 13:34:35 +0200

    Delete Adding a button.py

--- Adding a button.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root = Tk()
-def myClick():
-    myLabel = Label(root, text="Button Clicked")
-    myLabel.pack()
-
-myButton = Button(root, text= "Click Me!", padx=40, fg="blue", bg='yellow', command = myClick) #state = Disabled
-myButton.pack()
-root.mainloop()
